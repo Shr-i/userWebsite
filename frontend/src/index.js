0feabf3121a8026ab78efd8f300362d5e78db855
Shruti <shrutigour00@gmail.com>
2023-03-23 03:11:15 +0530

    fix: router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,15 @@ import { Provider } from "react-redux";
 import store from "./Store";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
